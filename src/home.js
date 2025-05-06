dfc79826c0af1c3e372f977e7f41555c00ea882c
Mohammadrasool Vahid <121235436+mutopia7@users.noreply.github.com>
2025-05-06 18:46:01 +0330

    Add home page

--- src/home.js
+++ src/home.js
@@ -1,8 +1,12 @@
 import "./styles.css"
+import "./home.css"
 
 function createHomePage(){
     const mainContent = document.querySelector("#content");
     mainContent.innerHTML = "";
+    const explain = document.createElement("p");
+    explain.textContent = "In this restaurant, the price of meals is not in dollars, but in the number of pages of a book that the customer has read."
+    mainContent.append(explain)
     
 }
 
--- src/index.js
+++ src/index.js
@@ -2,4 +2,16 @@ import { createHomePage} from "./home.js"
 
 createHomePage()
 
+const home = document.querySelector("#home");
+const menu = document.querySelector("#menu");
+const about = document.querySelector("#about");
+
+home.addEventListener("click", ()=> {
+    createHomePage()
+});
+
+
+
+
+
 console.log("webpack is runed")
